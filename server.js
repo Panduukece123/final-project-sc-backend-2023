4e5cf55c8f448ea798e8f0fcc6ecbdb81a85038d
Panduukece123 <148352274+Panduukece123@users.noreply.github.com>
2023-12-01 15:26:37 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-import express from "express";
-import mongoose from "mongoose";
-import configDB from "./config/dbConfig.js";
-import router from "./src/router.js";
+const express = require("express");
+const cors = require("cors");
+const db = require("./app/models");
 const app = express();
 
-//middleware
-app.use(express.json());
-app.use(router);
+const corsOptions = {
+    origin: "*" // Atau ganti dengan domain jika ingin menerima permintaan dari domain tertentu
+};
 
-// Connect to MongoDB
-mongoose.set("strictQuery", false);
-mongoose
-    .connect(configDB.url)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log(err.message);
+// Menerapkan middleware 
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Konek ke database;
+
+const mongooseConfig = {};
+
+db.mongoose.connect(db.url, mongooseConfig)
+    .then(() => console.log("database connected"))
+    .catch(err => {
+        console.log(`gagal konek ${err.message}`);
         process.exit(1);
     });
 
-//connect to server
-const port = 3000 || process.env.PORT;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// memanggil route tiket
+require("./app/routes/tiket_kereta.route")(app);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
